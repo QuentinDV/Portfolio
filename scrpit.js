c56cfdd438f9f09753f2c48794d0d8ab7bd09389
QuentinDV <dassivignonquentin@gmail.com>
2024-04-14 16:00:06 +0200

    avancement

--- scrpit.js
+++ scrpit.js
@@ -1,33 +1,15 @@
-let isLightMode = true; // Variable pour suivre l'état actuel de la couleur de fond
-
 document.getElementById("changeColorButton").addEventListener("click", function() {
-    if (isLightMode) {
-        // Changement vers le mode sombre
-        document.body.style.backgroundColor = "#F7F5F2";
-        document.querySelectorAll(".wrapper div").forEach(function(element) {
-            element.style.backgroundColor = "#D9D9D9";
-            element.style.color = "#101010";
-        });
-        // Modifier les chemins des images pour le mode sombre
-        document.querySelectorAll(".changetheme img").forEach(function(img) {
-            img.src = "assets/img/light/" + img.getAttribute("data-img");
-            console.log(img.src)
-        });
-    } else {
-        // Changement vers le mode clair
-        document.body.style.backgroundColor = "#D9D9D9";
-        document.querySelectorAll(".wrapper div").forEach(function(element) {
-            element.style.backgroundColor = "#F7F5F2";
-            element.style.color = "#101010";
-        });
-        // Modifier les chemins des images pour le mode clair
-        document.querySelectorAll(".changetheme img").forEach(function(img) {
-            img.src = "assets/img/dark/" + img.getAttribute("data-img");
-            console.log(img.src)
-        });
-    }
-    // Inverser l'état actuel
-    isLightMode = !isLightMode;
+    // Changement vers le mode clair
+    document.body.style.backgroundColor = "#D9D9D9";
+    document.querySelectorAll(".wrapper div").forEach(function(element) {
+        element.style.backgroundColor = "#F7F5F2";
+        element.style.color = "#101010";
+    });
+    // Modifier les chemins des images pour le mode clair
+    document.querySelectorAll(".changetheme img").forEach(function(img) {
+        img.src = "assets/img/dark/" + img.getAttribute("data-img");
+        console.log(img.src)
+    });
 });
 
 document.getElementById("resetColorButton").addEventListener("click", function() {
@@ -42,3 +24,4 @@ document.getElementById("resetColorButton").addEventListener("click", function()
         img.src = "assets/img/dark/" + img.getAttribute("data-img");
     });
 });
+
